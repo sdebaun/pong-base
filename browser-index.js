73e91e2377edae92ed056f06c569180fbff508e8
Stevo <sdebaun@sparks.network>
2015-05-25 09:52:44 -0700

    aha multiple main entries for bower

--- browser-index.js
+++
@@ -1,5 +0,0 @@
-angular.module('pong-base', ['firebase']);
-
-require('./browser/index');
-require('./shared/index');
-
---
+++ index.js
@@ -0,0 +1,4 @@
+require('pongular')
+.pongular
+.module('pong-base', [])
+.uses(['node/**/*.coffee','shared/**/*.coffee']);
